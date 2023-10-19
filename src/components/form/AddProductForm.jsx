db12b9e377524218ec15eec85bbd7ad0d2e20c0f
Stephen Busayo Adegoke <106432460+kuipid01@users.noreply.github.com>
2023-10-19 20:46:10 +0100

    Update AddProductForm.jsx

--- src/components/form/AddProductForm.jsx
+++ src/components/form/AddProductForm.jsx
@@ -94,10 +94,89 @@ const AddProductForm = ({ setFormOpen }) => {
       <ToastContainer />
       {
         loading ? <Loader /> :
-          <form className="form" onSubmit={handleSubmit}>
-            {/* ... (your input fields and other form elements) ... */}
-            <button className='formSubmitBtn' type="submit" style={{ marginTop: '20px' }}>Add Product</button>
-          </form>
+           <form className="form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="title"
+        placeholder="Title"
+        value={title}
+        className='form'
+        onChange={(e) => setTitle(e.target.value)}
+      /> 
+
+      <input
+        type="file"
+          className='form'
+        name="coverImage"
+        placeholder="Cover Image URL"
+        onChange={(e) => setCoverImage(e.target.files[0])}
+      /> 
+
+      <textarea
+        name="description"
+        placeholder="Description"
+        className='form'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+{/* {error && <h1>{error}</h1>} */}
+      <input
+        type="number"
+        name="price"
+        placeholder="Price"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      /> 
+
+      <input
+        type="number"
+        name="deliveryFee"
+        placeholder="Delivery Fee"
+        value={deliveryFee}
+        onChange={(e) => setDeliveryFee(e.target.value)}
+      />
+      <select
+        name="category"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      >
+        <option value="">Select a Category</option>
+        <option value="Carpentery">Carpentery</option>
+        <option value="Tailoring">Tailoring</option>
+        <option value="Furniture">Furniture</option>
+        <option value="Electrician">Electrician</option>
+        <option value="Plumber">Plumber</option>
+        <option value="HairDressing">Hair Dressing</option>
+        <option value="Others">Others</option>
+      </select>
+      <div className="images">
+        <label htmlFor="otherImages"> Image To be added </label>
+        {otherImages && (
+          <div className="listImage"> 
+          <AiOutlineClose onClick={() => setOtherImages([])} className="closeIcon" />
+            {otherImages.map((img, i) => {
+              return (
+                <div key={i}>
+                           <p >{img.name}  </p>
+                           <hr />
+                </div>
+          
+              );
+            })}
+          </div>
+        )}
+        <input
+          type="file"
+          name="otherImages"
+          placeholder="Other Images"
+          multiple // Add the 'multiple' attribute to accept multiple images
+          onChange={(e) => handleOtherImagesChange(e.target.files[0])}
+          accept="image/*"
+        />
+      </div> 
+
+      <button className='formSubmitBtn' type="submit" style={{marginTop:'20px'}}>Add Product</button>
+    </form>
       }
     </div>
   );
