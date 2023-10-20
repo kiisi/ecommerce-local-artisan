b7e92f2c40c3ec9fc6595e34c4a5aba879948a0c
Stephen Busayo Adegoke <106432460+kuipid01@users.noreply.github.com>
2023-10-20 13:17:17 +0100

    Update CartPage.jsx

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -67,9 +67,9 @@ const CartPage = () => {
             -
            
           </button>{" "}
-<<
-          <span className="qty1">{product?.qty} 🤝</span>{" "}
->>
+
+          <span className="qty1">{product?.qty} </span>{" "}
+
           <button
             className="qty"
              onClick={() => handleIncreaseCartQty(product)}
